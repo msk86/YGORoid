08bb7dd9f7229ebcb58ce38a37db6976424b7da3
Fan Wenbo <xbfan@thoughtworks.com>
2012-12-09 11:54:51 +0800

    new android project

---
+++ src/android/ygo/YGOActivity.java
@@ -0,0 +1,15 @@
+package android.ygo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YGOActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
