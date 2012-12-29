5634e77900a9174c8e887e96bf92b6e0a273906d
Fan Wenbo <xbfan@thoughtworks.com>
2012-12-29 13:05:58 +0800

    extract a base class for Deck/Grav...

---
+++ src/android/ygo/core/CardList.java
@@ -0,0 +1,89 @@
+package android.ygo.core;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.ygo.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CardList implements SelectableItem {
+    List<Card> cards;
+    boolean open = false;
+
+    public CardList(List<String> cardIds) {
+        cards = new ArrayList<Card>();
+        for (String id : cardIds) {
+            cards.add(new Card(id));
+        }
+    }
+
+    public Card pop() {
+        return cards.remove(0);
+    }
+
+    public void push(Card card) {
+        if(open) {
+            card.open();
+        } else {
+            card.set();
+        }
+        card.positive();
+        cards.add(0, card);
+    }
+
+    public void unShift(Card card) {
+        if(open) {
+            card.open();
+        } else {
+            card.set();
+        }
+        card.positive();
+        cards.add(card);
+    }
+
+
+    @Override
+    public Bitmap highLight() {
+        int height = Utils.cardHeight();
+        int width = Utils.cardWidth();
+        Bitmap highLightBmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(highLightBmp);
+        Paint paint = new Paint();
+        canvas.drawColor(Color.TRANSPARENT);
+        paint.setColor(Configuration.highlightColor());
+        paint.setStrokeWidth(4);
+        canvas.drawLine(0, 0, width, 0, paint);
+        canvas.drawLine(width, 0, width, height, paint);
+        canvas.drawLine(width, height, 0, height, paint);
+        canvas.drawLine(0, height, 0, 0, paint);
+        return highLightBmp;
+    }
+
+    @Override
+    public Bitmap toBitmap() {
+        Bitmap deckBmp;
+        if(cards.size() > 0) {
+            deckBmp = cards.get(0).toBitmap();
+        } else {
+            deckBmp = Bitmap.createBitmap(Utils.cardWidth(), Utils.cardHeight(), Bitmap.Config.ARGB_8888);
+        }
+        Canvas canvas = new Canvas(deckBmp);
+
+        CharSequence cs = "" + cards.size();
+        TextPaint textPaint = new TextPaint();
+        textPaint.setColor(Color.WHITE);
+
+        canvas.translate(0, Utils.cardHeight() - 20);
+        StaticLayout layout = new StaticLayout(cs, textPaint, Utils.cardWidth(), Layout.Alignment.ALIGN_CENTER, 0,0,false);
+        layout.draw(canvas);
+        return deckBmp;
+    }
+}
