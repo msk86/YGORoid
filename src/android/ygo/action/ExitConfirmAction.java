3d3399269d6239070ccb6a1f831f3c862ffd6f1f
Fan Wenbo <xbfan@thoughtworks.com>
2013-06-27 11:42:55 +0800

    remove unused ExitComfirm Class

--- src/android/ygo/action/ExitConfirmAction.java
+++ src/android/ygo/action/ExitConfirmAction.java
@@ -1,7 +1,9 @@
 package android.ygo.action;
 
-import android.ygo.core.ExitConfirm;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.ygo.op.Operation;
+import android.ygo.utils.Utils;
 
 public class ExitConfirmAction extends BaseAction {
     public ExitConfirmAction(Operation operation) {
@@ -10,6 +12,29 @@ public class ExitConfirmAction extends BaseAction {
 
     @Override
     public void execute() {
-    	new ExitConfirm();
+        AlertDialog dialog = new AlertDialog.Builder(Utils.getContext())
+                .setTitle("确定要退出程序吗？")
+                .setPositiveButton("确定", new OnExitClickListener("OK"))
+                .setNegativeButton("取消", new OnExitClickListener("Cancel"))
+                .create();
+        dialog.show();
+    }
+
+    private static class OnExitClickListener implements DialogInterface.OnClickListener {
+        private String button;
+
+        public OnExitClickListener(String button) {
+            this.button = button;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            if ("OK".equals(button)) {
+                Utils.getContext().stopService();
+
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+            }
+        }
     }
 }
