9ee13d66effb53c7e5bef36627f9f6b9152bb26f
Fan Wenbo <xbfan@thoughtworks.com>
2013-06-29 19:14:57 +0800

    add interface to show simple tip

--- src/android/ygo/YGOActivity.java
+++ src/android/ygo/YGOActivity.java
@@ -7,10 +7,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.WindowManager;
+import android.view.*;
+import android.webkit.WebView;
 import android.ygo.exception.CrashHandler;
 import android.ygo.service.PersistencyService;
 import android.ygo.utils.Utils;
@@ -21,7 +19,9 @@ import android.ygo.views.PlayOnKeyProcessor;
 public class YGOActivity extends Activity {
     private PlayOnKeyProcessor keyProcessor;
     private PlayMenuProcessor menuProcessor;
+    private View currentView;
     private DuelDiskView duelDiskView;
+    private WebView tipView;
 
     private PersistencyService service;
     private ServiceConnection sConn;
@@ -40,13 +40,30 @@ public class YGOActivity extends Activity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.main);
-        duelDiskView = (DuelDiskView) findViewById(R.id.duelDiskView);
+        duelDiskView = new DuelDiskView(this, null);
+        tipView = new WebView(this);
+        tipView.loadUrl("http://www.baidu.com");
+
+        setContentView(duelDiskView);
 
         keyProcessor = new PlayOnKeyProcessor(duelDiskView);
         menuProcessor = new PlayMenuProcessor(duelDiskView);
 
         startService();
+    }
+
+    @Override
+    public void setContentView(View view) {
+        currentView = view;
+        super.setContentView(view);
+    }
+
+    public void showTips() {
+        setContentView(tipView);
+    }
+
+    public void showDuel() {
+        setContentView(duelDiskView);
     }
 
     public boolean isMirror() {
@@ -59,18 +76,35 @@ public class YGOActivity extends Activity {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        return menuProcessor.onMenuPrepare(menu);
+        if(currentView == tipView) {
+            return super.onPrepareOptionsMenu(menu);
+        } else {
+            return menuProcessor.onMenuPrepare(menu);
+        }
     }
 
     @Override
     public boolean onMenuItemSelected(int id, MenuItem menuItem) {
-        return menuProcessor.onMenuClick(menuItem);
+        if(currentView == tipView) {
+            return super.onMenuItemSelected(id, menuItem);
+        } else {
+            return menuProcessor.onMenuClick(menuItem);
+        }
     }
 
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return keyProcessor.onKey(keyCode, event);
+        if(currentView == tipView) {
+            if(keyCode == KeyEvent.KEYCODE_BACK) {
+                showDuel();
+                return true;
+            } else {
+                return super.onKeyDown(keyCode, event);
+            }
+        } else {
+            return keyProcessor.onKey(keyCode, event);
+        }
     }
 
     @Override
@@ -105,7 +139,7 @@ public class YGOActivity extends Activity {
         return duelDiskView;
     }
 
-    public class PersistencyConnection implements ServiceConnection {
+    private class PersistencyConnection implements ServiceConnection {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             service = ((PersistencyService.MyBinder) iBinder).getService();
