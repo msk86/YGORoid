a42b5cfcc8509eb7e54d9b4cfef7d4b37e2aee24
Fan Wenbo <xbfan@thoughtworks.com>
2013-07-24 23:51:45 +0800

    fix 2 card xyz drag revert

--- src/android/ygo/action/RevertDragAction.java
+++ src/android/ygo/action/RevertDragAction.java
@@ -22,12 +22,20 @@ public class RevertDragAction extends BaseAction {
         StartDrag startDrag = drag.getStartDrag();
         Item from = startDrag.getContainer();
         if (from instanceof Field) {
-            SelectableItem fieldItem = ((Field) from).getItem();
+            Field field = (Field) from;
+            SelectableItem fieldItem = field.getItem();
             if (fieldItem != null) {
+                Card card = (Card) item;
                 if (fieldItem instanceof CardList) {
-                    ((CardList) fieldItem).push((Card) item);
+                    ((CardList) fieldItem).push(card);
                 } else if (fieldItem instanceof OverRay) {
-                    ((OverRay) fieldItem).overRay((Card) item);
+                    ((OverRay) fieldItem).overRay(card);
+                } else if (fieldItem instanceof Card) {
+                    Card targetCard = (Card) fieldItem;
+                    field.removeItem();
+                    OverRay overRay = new OverRay(targetCard);
+                    overRay.overRay(card);
+                    field.setItem(overRay);
                 }
             } else {
                 ((Field) from).setItem(item);
