5a23c0a9246afbcd3f179afb805d5b4477f10fcc
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-11 09:57:20 +0800

    confirm when enter deck builder

--- src/android/ygo/action/DeckBuilderAction.java
+++ src/android/ygo/action/DeckBuilderAction.java
@@ -1,8 +1,7 @@
 package android.ygo.action;
 
-import android.ygo.core.Card;
-import android.ygo.core.SideWindow;
-import android.ygo.layout.Layout;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.ygo.op.Operation;
 import android.ygo.utils.Utils;
 
@@ -13,7 +12,31 @@ public class DeckBuilderAction extends BaseAction {
 
     @Override
     public void execute() {
-        String deck = duel.getDeckName();
-        Utils.getContext().showDeckBuilderWithDeck(deck);
+        if (duel.getDeckName() != null) {
+            AlertDialog dialog = new AlertDialog.Builder(Utils.getContext())
+                    .setTitle("放弃决斗开始组卡？")
+                    .setPositiveButton("确定", new OnDeckBuilderClickListener("OK"))
+                    .setNegativeButton("取消", new OnDeckBuilderClickListener("Cancel"))
+                    .create();
+            dialog.show();
+        } else {
+            Utils.getContext().showDeckBuilderWithDeck(null);
+        }
+    }
+
+    private class OnDeckBuilderClickListener implements DialogInterface.OnClickListener {
+        private String button;
+
+        public OnDeckBuilderClickListener(String button) {
+            this.button = button;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            if ("OK".equals(button)) {
+                String deck = duel.getDeckName();
+                Utils.getContext().showDeckBuilderWithDeck(deck);
+            }
+        }
     }
 }
