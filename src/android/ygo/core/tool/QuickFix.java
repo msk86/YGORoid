97b3728d9c14880f8e363194abb919f6a0c27776
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-11 10:59:15 +0800

    fix gesi token

--- src/android/ygo/core/tool/QuickFix.java
+++ src/android/ygo/core/tool/QuickFix.java
@@ -6,6 +6,10 @@ import android.ygo.core.Const;
 public class QuickFix {
 
     public static void fix(Card card) {
+        // 格斯
+        if("44330098".equals(card.getRealId())) {
+            card.setCategory(Const.CATEGORY_TOKEN);
+        }
         // 替罪羊
         if("73915051".equals(card.getRealId())) {
             card.setCategory(Const.CATEGORY_TOKEN);
