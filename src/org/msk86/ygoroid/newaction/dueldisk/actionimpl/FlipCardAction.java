12a80a2e78277a8a6ad9ed9d1ae652fda1032411
Fan Wenbo <xbfan@thoughtworks.com>
2014-03-23 12:01:53 +0800

    select item when flip and rotate

--- src/org/msk86/ygoroid/newaction/dueldisk/actionimpl/FlipCardAction.java
+++ src/org/msk86/ygoroid/newaction/dueldisk/actionimpl/FlipCardAction.java
@@ -1,6 +1,9 @@
 package org.msk86.ygoroid.newaction.dueldisk.actionimpl;
 
 import org.msk86.ygoroid.newcore.Controllable;
+import org.msk86.ygoroid.newcore.Selectable;
+import org.msk86.ygoroid.newcore.constant.FieldType;
+import org.msk86.ygoroid.newcore.impl.Field;
 import org.msk86.ygoroid.newop.Operation;
 
 public class FlipCardAction extends BaseAction {
@@ -10,8 +13,13 @@ public class FlipCardAction extends BaseAction {
 
     @Override
     public void execute() {
-        if(item  instanceof Controllable) {
-            ((Controllable) item).flip();
+        if(item instanceof Controllable) {
+            Controllable controllable = (Controllable) item;
+            controllable.flip();
+
+            if(item instanceof Selectable) {
+                duel.select((Selectable) item);
+            }
         }
     }
 }
--- src/org/msk86/ygoroid/newaction/dueldisk/actionimpl/RotateMonsterPositionAction.java
+++ src/org/msk86/ygoroid/newaction/dueldisk/actionimpl/RotateMonsterPositionAction.java
@@ -1,6 +1,7 @@
 package org.msk86.ygoroid.newaction.dueldisk.actionimpl;
 
 import org.msk86.ygoroid.newcore.Controllable;
+import org.msk86.ygoroid.newcore.Selectable;
 import org.msk86.ygoroid.newop.Operation;
 
 public class RotateMonsterPositionAction extends BaseAction {
@@ -12,6 +13,10 @@ public class RotateMonsterPositionAction extends BaseAction {
     public void execute() {
         if (item instanceof Controllable) {
             ((Controllable) item).rotate();
+
+            if(item instanceof Selectable) {
+                duel.select((Selectable) item);
+            }
         }
     }
 }
