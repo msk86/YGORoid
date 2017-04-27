56c02130352c625a4d16e87d4ee60c4888df94b6
duelqiuqiu <duelqiuqiu@gmail.com>
2017-04-27 18:51:57 +0800

    Update FieldType.java

--- src/org/msk86/ygoroid/newcore/constant/FieldType.java
+++ src/org/msk86/ygoroid/newcore/constant/FieldType.java
@@ -3,5 +3,9 @@ package org.msk86.ygoroid.newcore.constant;
 public enum FieldType {
     MONSTER, MAGIC_TRAP, FIELD_MAGIC, DECK, EX_DECK,
     GRAVEYARD, BANISHED, TEMP, EX_MONSTER;
+    
+    public String getDefaultBgResId() {
+		return null;
+    }
 }
 
